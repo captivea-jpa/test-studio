dca1e1dc5f7c6cade15af4b58306415bb679a55e
captivea-jpa <35844968+captivea-jpa@users.noreply.github.com>
2019-04-02 15:22:17 +0200

    Create __manifest__.py

---
+++ cap_res_partner/__manifest__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+{
+  'name': "cap_res_partner",
+  'version': "1.0",
+  'author': "captivea-jpa",
+  'depends': ["base"],
+  'installable': True
+}
